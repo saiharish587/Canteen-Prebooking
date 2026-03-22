488d994732d65cfe4ab6fff553134b4fe1ab65dc
manjunath-2043980 <malipatlollamanjunath@gmail.com>
2026-03-22 19:08:52 +0530

    Update to PHP 8.4 and remove composer.lock for fresh resolution

--- canteen-api/start.sh
+++ canteen-api/start.sh
@@ -4,6 +4,9 @@ set -e
 
 cd /app
 
+echo "Removing lock file for fresh composer resolution..."
+rm -f composer.lock
+
 echo "Installing Composer dependencies..."
 composer install --no-dev --no-interaction
 
