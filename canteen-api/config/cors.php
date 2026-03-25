3c24ba20fc083bb02caa3fb9a697973fab934876
manjunath-2043980 <malipatlollamanjunath@gmail.com>
2026-03-25 18:32:21 +0530

    Add port 5500 to localhost CORS origins for VS Code Live Server development

--- canteen-api/config/cors.php
+++ canteen-api/config/cors.php
@@ -17,12 +17,14 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        // Development environments
+        // Development environments - Local machine
         'http://localhost',
         'http://localhost:3000',
+        'http://localhost:5500',
         'http://localhost:8000',
         'http://127.0.0.1',
         'http://127.0.0.1:3000',
+        'http://127.0.0.1:5500',
         'http://127.0.0.1:8000',
         'http://localhost/canteen',
         
